3b1c9d69c3250508e2eb449f4cb06420aca0b9de
parameterized <troyk212@gmail.com>
2020-08-09 09:42:40 -0400

    update readme, change measure rules, multi win lines, bugfixes

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Qnect4
 
 A quantum gravity board game
+
+![](readme-assets/qnect4.png)
+
+## Running
+
+The lastest version will be hosted on Github [here](https://parameterized.github.io/qnect4).
+
+If you want to modify the code, install the dependencies (`npm install`) then `npm run dev-server` to start the webpack development server.
+
+## Gameplay
+
+Qnect4 is based on [Quantum tic-tac-toe](https://en.wikipedia.org/wiki/Quantum_tic-tac-toe). It shares the main mechanic of being able to place a move in 2 locations at once, but gravity will often cause a move to interact with other move superpositions and be split into more than just 2 possible locations. The rules for measurement are also different.
+
+### Measurement
+
+A player will be allowed to measure a cell (choose a disk to collapse its superposition to that cell) when the opposing player's turn leaves that cell either entangled with 4 disks, or in a state where it's no longer possible for it to be empty (as in the case of a bottom row cell if all superpositions of a disk exist in that column, or if a cell above contains a classical disk). Measurement of one cell may change the eligibility of other cells for measurement at that time.
+
+Placing both of your moves in the same column will immediately collapse to a classical disk (with definite location) if no spooky disks (disks that exist in multiple locations) are in that column.
+
+### Superposition and indirect collapse
+
+If your move falls onto another spooky disk, it will be split into two - one where it is stopped by that disk and one where it isn't. If the obstructing disk is later measured to be in another location, the spooky disk created by your move which was obstructed will effectively be measured to not exist in that location, and if there is only one remaining possible location, it will collapse and become a classical disk in that location.
+
+### Winning
+
+Four in a row (horizontally, vertically, or diagonally) of classical disks will win the game. If a single move causes both players get four in a row (I'm not sure if this is possible), the game will be tied.
+
+## Controls
+- Left click a column to drop one of your spooky disks into it
+- If a cell is potentially occupied by 4 different disks (and not supported by spooky disks), left click one of the disks to collapse it to that position
--- src/game.js
+++ src/game.js
@@ -9,14 +9,13 @@ let ny = 6;
 
 let varsLoaded = false;
 
-let bgCol, boardCol, p1Col, p2Col, highlightCol1, highlightCol2;
+let bgCol, boardCol, playerColors, highlightCol1, highlightCol2;
 function loadColors() {
     bgCol = color('#E7E7E7');
     boardCol = color('#FDFFFC');
-    p1Col = color('#C1292E');
-    p2Col = color('#235789');
     highlightCol1 = color('#539987');
     highlightCol2 = color('#539987'); highlightCol2.setAlpha(100);
+    playerColors = [color('#C1292E'), color('#235789'), highlightCol1]
 }
 
 let boardRect;
@@ -48,11 +47,11 @@ let ij2xy = (i, j) => [(pad + circleD) * i + pad + circleD / 2, (pad + circleD) 
 let onBoard = (i, j) => i >= 0 && i < nx && j >= 0 && j < ny;
 
 export class Game {
-    activePlayerId = 1; // 1, 2
+    activePlayerId = 0; // 0, 1
     turnNum = 1;
-    fullCells = []; // [[i, j], ...]
+    measurableCells = []; // [[i, j], ...]
     won = false;
-    winningPieces = [];
+    winningDisks = new Set();
     wonT = 0;
 
     constructor() {
@@ -75,7 +74,7 @@ export class Game {
     }
 
     newDisk() {
-        let c = this.activePlayerId === 1 ? p1Col : p2Col;
+        let c = playerColors[this.activePlayerId]
         colorMode(HSB);
         c = color(
             utils.mod(hue(c) + random(-10, 10), 360),
@@ -101,9 +100,6 @@ export class Game {
         disk.i = i, disk.j = j;
         let cell = this.boardState[i][j];
         cell.push(disk);
-        if (cell.length === 4) {
-            this.fullCells.push([i, j]);
-        }
     }
 
     removeDisk(disk) {
@@ -132,22 +128,19 @@ export class Game {
             let idx = cell.indexOf(disk);
             if (idx !== -1) {
                 cell.splice(idx, 1);
-                // if cell was full, remove from fullCells
-                for (let fci = 0; fci < this.fullCells.length; fci++) {
-                    let [i, j] = this.fullCells[fci];
-                    if (i === disk.i && j === disk.j) {
-                        this.fullCells.splice(fci, 1);
-                        break;
+                // remove disks stacked higher than can be supported
+                if (cell.length > 1 && cell[0].superposition.length > 0) {
+                    for (let j = disk.j - cell.length; j >= 0; j--) {
+                        let cell2 = this.boardState[disk.i][j];
+                        for (let cd of cell2) {
+                            this.removeDisk(cd);
+                        }
                     }
-                }
-                // if cell contains 1, remove any of remaining disk's superpositions that are above
-                if (cell.length === 1) {
-                    let cd = cell[0];
-                    if (cd.j - 1 >= 0) {
-                        let cell2 = this.boardState[cd.i][cd.j - 1];
-                        let idx2 = cell2.findIndex(d => d.turnNum === cd.turnNum);
-                        if (idx2 !== -1) {
-                            this.removeDisk(cell2[idx2]);
+                } else if (cell.length === 1 && disk.j > 0) {
+                    let cell2 = this.boardState[disk.i][disk.j - 1];
+                    for (let cd of cell2) {
+                        if (cd.turnNum === cell[0].turnNum) {
+                            this.removeDisk(cd);
                         }
                     }
                 }
@@ -162,7 +155,7 @@ export class Game {
 
     checkNextTurn() {
         if (this.activeDisks.length === 0) {
-            this.activePlayerId = this.activePlayerId % 2 + 1;
+            this.activePlayerId = (this.activePlayerId + 1) % 2;
             this.turnNum++;
             this.loadActiveDisks();
         }
@@ -173,6 +166,57 @@ export class Game {
         return constrain(floor((mx - boardRect.x - pad / 2) / (pad + circleD)), 0, nx - 1);
     }
 
+    maxEmptyInColumn(i) {
+        let checkedDisks = new Set();
+        let minDisksInColumn = 0;
+        for (let j = ny - 1; j >= 0; j--) {
+            let cell = this.boardState[i][j];
+            if (cell.length === 0) {
+                break;
+            }
+            for (let disk of cell) {
+                if (!checkedDisks.has(disk.turnNum)) {
+                    let allInColumn = true;
+                    for (let d2 of disk.superposition) {
+                        if (d2.i !== i) {
+                            allInColumn = false;
+                            break;
+                        }
+                    }
+                    if (allInColumn) {
+                        minDisksInColumn++;
+                    }
+                    checkedDisks.add(disk.turnNum);
+                }
+            }
+        }
+        let maxEmpty = ny - minDisksInColumn;
+        for (let j = 0; j < maxEmpty; j++) {
+            let cell = this.boardState[i][j];
+            if (cell.length > 0 && cell[0].superposition.length === 0) { // has classical
+                return j;
+            }
+        }
+        return maxEmpty;
+    }
+
+    updateMeasurableCells() {
+        this.measurableCells = [];
+        for (let i = 0; i < nx; i++) {
+            let n = this.maxEmptyInColumn(i);
+            for (let j = ny - 1; j >= 0; j--) {
+                let cell = this.boardState[i][j];
+                if (cell.length === 1 && cell[0].superposition.length === 0) { // classical
+                    continue;
+                }
+                if (j < n && cell.length !== 4) { // cell can be empty and doesn't have 4 spooky disks
+                    break;
+                }
+                this.measurableCells.push([i, j]);
+            }
+        }
+    }
+
     checkWon(i, j) {
         let cell = this.boardState[i][j];
         if (cell.length !== 1) {
@@ -181,7 +225,7 @@ export class Game {
         let playerId = cell[0].playerId;
         for (let dir of [[0, 1], [1, 1], [1, 0], [1, -1]]) {
             let consecutive = 1;
-            let winningPieces = [this.boardState[i][j][0]];
+            let possibleWinningDisks = [this.boardState[i][j][0]];
             for (let n = 1; n <= 3; n++) { // check + dir
                 let ni = i + dir[0] * n;
                 let nj = j + dir[1] * n;
@@ -189,7 +233,7 @@ export class Game {
                 let c2 = this.boardState[ni][nj];
                 if (c2.length === 1 && c2[0].playerId === playerId && c2[0].superposition.length === 0) {
                     consecutive++;
-                    winningPieces.push(c2[0]);
+                    possibleWinningDisks.push(c2[0]);
                 } else {
                     break;
                 }
@@ -201,15 +245,21 @@ export class Game {
                 let c2 = this.boardState[ni][nj];
                 if (c2.length === 1 && c2[0].playerId === playerId && c2[0].superposition.length === 0) {
                     consecutive++;
-                    winningPieces.push(c2[0]);
+                    possibleWinningDisks.push(c2[0]);
                 } else {
                     break;
                 }
             }
             if (consecutive >= 4) {
                 this.won = true;
-                this.wonPlayer = playerId;
-                this.winningPieces = winningPieces;
+                if (this.wonPlayer !== undefined && this.wonPlayer !== playerId) {
+                    this.wonPlayer = 2;
+                } else {
+                    this.wonPlayer = playerId;
+                }
+                for (let disk of possibleWinningDisks) {
+                    this.winningDisks.add(disk);
+                }
             }
         }
     }
@@ -219,40 +269,46 @@ export class Game {
             if (this.wonT > 1) {
                 resetGame();
             }
-        } else if (this.fullCells.length > 0 && this.activeDisks.length === 2) {
-            // choose collapse if has full cells and not placing 2nd disk
-            for (let [i, j] of this.fullCells) {
-                if (!onBoard(i, j + 1) || this.boardState[i][j + 1][0].superposition.length === 0) {
-                    let [cx, cy] = ij2xy(i, j);
-                    let cell = this.boardState[i][j];
-                    let diskClicked = false;
-                    for (let k = 0; k < cell.length; k++) {
-                        let x = cx - circleD / 4 + circleD / 2 * (k % 2);
-                        let y = cy + circleD / 4 - circleD / 2 * floor(k / 2);
-                        if (utils.mouseInCircle(boardRect.x + x, boardRect.y + y, circleD / 2)) {
-                            diskClicked = true;
-                            // collapse to selected disk (remove its superpositions)
-                            // other disks in cell will be removed when disk has 0 superpositions
-                            let disk = cell[k];
-                            while (disk.superposition.length > 0) {
-                                this.removeDisk(disk.superposition[0]);
-                            }
-                            sfx.collapse.play();
-                            break;
+        } else if (this.measurableCells.length > 0 && this.activeDisks.length === 2) {
+            // measure cell
+            for (let [i, j] of this.measurableCells) {
+                let [cx, cy] = ij2xy(i, j);
+                let cell = this.boardState[i][j];
+                let measured = false;
+                for (let k = 0; k < cell.length; k++) {
+                    let x = cx - circleD / 4 + circleD / 2 * (k % 2);
+                    let y = cy + circleD / 4 - circleD / 2 * floor(k / 2);
+                    if (utils.mouseInCircle(boardRect.x + x, boardRect.y + y, circleD / 2)) {
+                        // collapse to selected disk (remove its superpositions)
+                        // other disks in cell will be removed by removeDisk when disk has 0 superpositions
+                        let disk = cell[k];
+                        while (disk.superposition.length > 0) {
+                            this.removeDisk(disk.superposition[0]);
                         }
+                        measured = true;
+                        break;
                     }
-                    if (diskClicked) { break; }
+                }
+                if (measured) {
+                    this.updateMeasurableCells();
+                    sfx.collapse.play();
+                    break;
                 }
             }
         } else if (utils.mouseInRect(boardRect)) {
+            // place disk
             let i = this.getHovered();
-            for (let j = ny - 1; j >= 0; j--) {
+            let j0 = this.maxEmptyInColumn(i) - 1;
+            let diskPlaced = false;
+            for (let j = j0; j >= 0; j--) {
                 let cell = this.boardState[i][j];
                 if (cell.length === 0) { // clear
                     this.addDisk(this.activeDisks.shift(), i, j);
+                    diskPlaced = true;
                     break;
-                } else if (cell[0].superposition.length > 0) { // has spooky (cells above either spooky or clear)
+                } else if (cell[0].superposition.length > 0) { // has spooky disk
                     let idx = cell.findIndex(disk => disk.turnNum === this.activeDisks[0].turnNum);
+                    diskPlaced = true;
                     if (idx === -1) {
                         if (j === 0) {
                             this.addDisk(this.activeDisks.shift(), i, j);
@@ -265,8 +321,11 @@ export class Game {
                     }
                 }
             }
-            this.checkNextTurn();
-            sfx.hit.play();
+            if (diskPlaced) {
+                this.updateMeasurableCells();
+                this.checkNextTurn();
+                sfx.hit.play();
+            }
         }
     }
 
@@ -277,26 +336,37 @@ export class Game {
     }
 
     draw() {
+        // win bg
         if (this.won) {
             let t = ease.outQuad(min(this.wonT, 1)) * 0.3;
-            background(lerpColor(bgCol, this.wonPlayer === 1 ? p1Col : p2Col, t));
+            background(lerpColor(bgCol, playerColors[this.wonPlayer], t));
         } else {
             background(bgCol);
         }
 
+        // title
         fill('#08090A');
         textAlign(CENTER, CENTER);
         textFont('Georgia');
         textSize(56);
         text('Qnect4', targetWidth / 2, 40);
 
-        textSize(40);
-        if (this.won) {
-            fill(this.wonPlayer === 1 ? p1Col : p2Col);
-            text(`${this.wonPlayer === 1 ? 'Red' : 'Blue'} Won!`, targetWidth - boardRect.x / 2, 250);
-        } else {
-            fill(this.activePlayerId === 1 ? p1Col : p2Col);
-            text(`${this.activePlayerId === 1 ? 'Red' : 'Blue'}'s turn`, targetWidth - boardRect.x / 2, 250);
+        // game state message
+        {
+            textSize(40);
+            let x = targetWidth - boardRect.x / 2;
+            let y = 250;
+            if (this.won) {
+                fill(playerColors[this.wonPlayer]);
+                if (this.wonPlayer === 2) {
+                    text('Game Tied!', x, y);
+                } else {
+                    text(`${this.wonPlayer === 0 ? 'Red' : 'Blue'} Won!`, x, y);
+                }
+            } else {
+                fill(playerColors[this.activePlayerId]);
+                text(`${this.activePlayerId === 0 ? 'Red' : 'Blue'}'s turn`, x, y);
+            }
         }
 
         push();
@@ -333,27 +403,26 @@ export class Game {
         image(boardImage, 0, 0);
 
         strokeWeight(3);
-        if (this.fullCells.length > 0 && this.activeDisks.length === 2) {
+        if (this.measurableCells.length > 0 && this.activeDisks.length === 2) {
             // outline collapsible full cells
             stroke(highlightCol1);
             noFill();
-            for (let [i, j] of this.fullCells) {
-                if (!onBoard(i, j + 1) || this.boardState[i][j + 1][0].superposition.length === 0) {
-                    let [cx, cy] = ij2xy(i, j);
-                    for (let k = 0; k < this.boardState[i][j].length; k++) {
-                        let x = cx - circleD / 4 + circleD / 2 * (k % 2);
-                        let y = cy + circleD / 4 - circleD / 2 * floor(k / 2);
-                        noFill();
-                        if (utils.mouseInCircle(boardRect.x + x, boardRect.y + y, circleD / 2)) {
-                            utils.setPointer();
-                            fill(highlightCol2);
-                        }
-                        circle(x, y, circleD / 2);
+            for (let [i, j] of this.measurableCells) {
+                let [cx, cy] = ij2xy(i, j);
+                for (let k = 0; k < this.boardState[i][j].length; k++) {
+                    let x = cx - circleD / 4 + circleD / 2 * (k % 2);
+                    let y = cy + circleD / 4 - circleD / 2 * floor(k / 2);
+                    noFill();
+                    if (utils.mouseInCircle(boardRect.x + x, boardRect.y + y, circleD / 2)) {
+                        utils.setPointer();
+                        fill(highlightCol2);
                     }
+                    circle(x, y, circleD / 2);
                 }
             }
             noStroke();
         } else {
+            // show placement guide
             let i = this.getHovered();
             if (!this.won && utils.mouseInRect(boardRect) && (this.boardState[i][0].length === 0 || this.boardState[i][0][0].superposition.length > 0)) {
                 utils.setPointer();
@@ -381,11 +450,11 @@ export class Game {
             }
         }
 
-        // outline winning pieces
+        // outline winning disks
         if (this.won) {
             stroke(highlightCol1);
             fill(highlightCol2)
-            for (let disk of this.winningPieces) {
+            for (let disk of this.winningDisks) {
                 circle(...ij2xy(disk.i, disk.j), circleD);
             }
             noStroke();
